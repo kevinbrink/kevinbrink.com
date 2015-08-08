ad24280ca86544e61469b8a9c379159fa9ff59fd
Kevin Brink <kevin@kevinbrink.com>
2015-08-08 18:42:58 -0400

    Add static pages. Remove unneeded actions for blog_posts. Begin authentication

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,5 +1,6 @@
 class BlogPostsController < ApplicationController
   before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate, only: [:new, :create]
 
   # GET /blog_posts
   # GET /blog_posts.json
@@ -7,18 +8,9 @@ class BlogPostsController < ApplicationController
     @blog_posts = BlogPost.all
   end
 
-  # GET /blog_posts/1
-  # GET /blog_posts/1.json
-  def show
-  end
-
   # GET /blog_posts/new
   def new
     @blog_post = BlogPost.new
-  end
-
-  # GET /blog_posts/1/edit
-  def edit
   end
 
   # POST /blog_posts
@@ -26,40 +18,48 @@ class BlogPostsController < ApplicationController
   def create
     @blog_post = BlogPost.new(blog_post_params)
 
-    respond_to do |format|
-      if @blog_post.save
-        format.html { redirect_to @blog_post, notice: 'Blog post was successfully created.' }
-        format.json { render :show, status: :created, location: @blog_post }
-      else
-        format.html { render :new }
-        format.json { render json: @blog_post.errors, status: :unprocessable_entity }
-      end
+    if @blog_post.save
+      redirect_to root_path, notice: 'Blog post was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /blog_posts/1
-  # PATCH/PUT /blog_posts/1.json
-  def update
-    respond_to do |format|
-      if @blog_post.update(blog_post_params)
-        format.html { redirect_to @blog_post, notice: 'Blog post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @blog_post }
-      else
-        format.html { render :edit }
-        format.json { render json: @blog_post.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /blog_posts/1
-  # DELETE /blog_posts/1.json
-  def destroy
-    @blog_post.destroy
-    respond_to do |format|
-      format.html { redirect_to blog_posts_url, notice: 'Blog post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+################## CURRENTLY UNSUPPORTED OPERATIONS ############
+#
+#  # PATCH/PUT /blog_posts/1
+#  # PATCH/PUT /blog_posts/1.json
+#  def update
+#    respond_to do |format|
+#      if @blog_post.update(blog_post_params)
+#        format.html { redirect_to @blog_post, notice: 'Blog post was successfully updated.' }
+#        format.json { render :show, status: :ok, location: @blog_post }
+#      else
+#        format.html { render :edit }
+#        format.json { render json: @blog_post.errors, status: :unprocessable_entity }
+#      end
+#    end
+#  end
+#  
+#  # GET /blog_posts/1
+#  # GET /blog_posts/1.json
+#  def show
+#  end
+#
+#
+#  # DELETE /blog_posts/1
+#  # DELETE /blog_posts/1.json
+#  def destroy
+#    @blog_post.destroy
+#    respond_to do |format|
+#      format.html { redirect_to blog_posts_url, notice: 'Blog post was successfully destroyed.' }
+#      format.json { head :no_content }
+#    end
+#  end
+#
+#  # GET /blog_posts/1/edit
+#  def edit
+#  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -70,5 +70,8 @@ class BlogPostsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_post_params
       params.require(:blog_post).permit(:title, :body, :tagline)
+    end
+
+    def authenticate
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   root 'blog_posts#index'
-  resources :blog_posts
+  get 'new_blog' => 'blog_posts#new'
+  post 'create_blog' => 'blog_posts#create'
+  post 'blog_posts' => 'blog_posts#create'
+  
+  #resources :blog_posts
+  #get 'new_blog_post' => 'blog_posts#new'
+  #get 'edit_blog_post' => ' blog_posts#edit'
+  #get 'blog_post' => ' blog_posts#show'
+  #patch  'blog_post' => ' blog_posts#update'
+  #put    'blog_post' => ' blog_posts#update'
+  #delete 'blog_post' => ' blog_posts#destroy'
+
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 end
