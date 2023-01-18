4254d27f0d1bac9807b9a10af5bcccf92fe71af8
kevinbrink <Kevin@Kevinbrink.com>
2023-01-17 19:52:47 -0500

    Attempt updated API endpoint

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const db = require('@cyclic.sh/dynamodb')
+const path = require('path');
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -20,44 +21,57 @@ app.use(express.urlencoded({ extended: true }))
 // #############################################################################
 
 // Create or Update an item
-app.post('/:col/:key', async (req, res) => {
-  console.log(req.body)
-
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
-
-// Delete an item
-app.delete('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).delete(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
-
-// Get a single item
-app.get('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).get(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
-
-// Get a full listing
-app.get('/:col', async (req, res) => {
+// app.post('/:col/:key', async (req, res) => {
+//   console.log(req.body)
+// 
+//   const col = req.params.col
+//   const key = req.params.key
+//   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
+//   const item = await db.collection(col).set(key, req.body)
+//   console.log(JSON.stringify(item, null, 2))
+//   res.json(item).end()
+// })
+// 
+// // Delete an item
+// app.delete('/:col/:key', async (req, res) => {
+//   const col = req.params.col
+//   const key = req.params.key
+//   console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
+//   const item = await db.collection(col).delete(key)
+//   console.log(JSON.stringify(item, null, 2))
+//   res.json(item).end()
+// })
+// 
+// // Get a single item
+// app.get('/:col/:key', async (req, res) => {
+//   const col = req.params.col
+//   const key = req.params.key
+//   console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
+//   const item = await db.collection(col).get(key)
+//   console.log(JSON.stringify(item, null, 2))
+//   res.json(item).end()
+// })
+// 
+// // Get a full listing
+app.get('/api/:col', async (req, res) => {
   const col = req.params.col
   console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
   const items = await db.collection(col).list()
   console.log(JSON.stringify(items, null, 2))
-  res.json(items).end()
+  const realerData = await Promise.all(
+    items.map(async ({ key }) => (await db.collection(col).get(key)).props)
+  );
+  console.log(JSON.stringify(realerData, null, 2))
+  res.json(realerData).end()
+})
+
+app.use("/static", express.static("client/build/static"));
+
+app.get('/', (req, res) => {
+  res.sendFile("index.html", {
+    root: path.join(__dirname, "client/build")
+  });
+  // res.send("<h1>Hello there</h1>");
 })
 
 // Catch all handler for all other request.
